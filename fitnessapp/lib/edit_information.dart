3c22b616dc0e2bc2794a2a4be263ea4914f8fa7c
Jero Diacuna <jerodiacuna@gmail.com>
2023-12-03 11:18:45 +0800

    activity changed, editinfo also

--- fitnessapp/lib/edit_information.dart
+++ fitnessapp/lib/edit_information.dart
@@ -20,13 +20,12 @@ class _EditInformationState extends State<EditInformation> {
   TextEditingController ageController = TextEditingController();
   TextEditingController heightController = TextEditingController();
   TextEditingController weightController = TextEditingController();
-  String? selectedSex; // Added for dropdown selection
-  bool userDeleted = false; // Track user deletion
+  String? selectedSex;
+  bool userDeleted = false;
 
   @override
   void initState() {
     super.initState();
-    // Fetch user information and populate the text fields
     fetchUserInfo();
   }
 
@@ -43,12 +42,11 @@ class _EditInformationState extends State<EditInformation> {
           ageController.text = doc['age'];
           heightController.text = doc['height'];
           weightController.text = doc['weight'];
-          selectedSex = doc['sex'] ?? ''; // Set selected sex if available
+          selectedSex = doc['sex'] ?? '';
         });
       }
     } catch (e) {
       print(e);
-      // Handle error while fetching data
     }
   }
 
@@ -62,13 +60,10 @@ class _EditInformationState extends State<EditInformation> {
         'age': ageController.text,
         'height': heightController.text,
         'weight': weightController.text,
-        'sex': selectedSex, // Include selectedSex information
+        'sex': selectedSex,
       });
-
-      // Optional: Show a success message or navigate back to the previous screen
     } catch (e) {
       print(e);
-      // Handle error updating information
     }
   }
 
@@ -82,31 +77,27 @@ class _EditInformationState extends State<EditInformation> {
           actions: <Widget>[
             TextButton(
               onPressed: () async {
-                Navigator.of(context).pop(); // Close the dialog
+                Navigator.of(context).pop();
                 try {
-                  // Delete user information document
                   await firestore
                       .collection('user_information')
                       .doc(widget.user.uid)
                       .delete();
 
-                  // Delete the user's Firebase authentication account
                   await widget.user.delete();
 
-                  // Navigate back to loginscreen after successful deletion
                   setState(() {
-                    userDeleted = true; // Update the user deletion status
+                    userDeleted = true;
                   });
                 } catch (e) {
                   print(e);
-                  // Handle error while deleting user or user information
                 }
               },
               child: const Text('Yes'),
             ),
             TextButton(
               onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
+                Navigator.of(context).pop();
               },
               child: const Text('No'),
             ),
@@ -137,113 +128,108 @@ class _EditInformationState extends State<EditInformation> {
           SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: IgnorePointer(
-                ignoring:
-                    userDeleted, // Disable interaction when userDeleted is true
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    TextField(
-                      controller: nameController,
-                      decoration: const InputDecoration(
-                        labelText: 'Name',
-                      ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
                     ),
-                    TextField(
-                      controller: ageController,
-                      decoration: const InputDecoration(
-                        labelText: 'Age',
-                      ),
+                  ),
+                  TextField(
+                    controller: ageController,
+                    decoration: const InputDecoration(
+                      labelText: 'Age',
                     ),
-                    DropdownButtonFormField<String>(
-                      value: selectedSex,
-                      onChanged: (newValue) {
-                        setState(() {
-                          selectedSex = newValue;
-                        });
-                      },
-                      items: ['Male', 'Female']
-                          .map((sex) => DropdownMenuItem<String>(
-                                value: sex,
-                                child: Text(sex),
-                              ))
-                          .toList(),
-                      decoration: const InputDecoration(
-                        labelText: 'Sex',
-                      ),
+                  ),
+                  DropdownButtonFormField<String>(
+                    value: selectedSex,
+                    onChanged: (newValue) {
+                      setState(() {
+                        selectedSex = newValue;
+                      });
+                    },
+                    items: ['Male', 'Female']
+                        .map((sex) => DropdownMenuItem<String>(
+                              value: sex,
+                              child: Text(sex),
+                            ))
+                        .toList(),
+                    decoration: const InputDecoration(
+                      labelText: 'Sex',
                     ),
-                    TextField(
-                      controller: heightController,
-                      decoration: const InputDecoration(
-                        labelText: 'Height(cm)',
-                      ),
+                  ),
+                  TextField(
+                    controller: heightController,
+                    decoration: const InputDecoration(
+                      labelText: 'Height(cm)',
                     ),
-                    TextField(
-                      controller: weightController,
-                      decoration: const InputDecoration(
-                        labelText: 'Weight(kg)',
-                      ),
+                  ),
+                  TextField(
+                    controller: weightController,
+                    decoration: const InputDecoration(
+                      labelText: 'Weight(kg)',
                     ),
-                    ElevatedButton(
-                      onPressed: updateInformation,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue.withOpacity(.9),
-                      ),
-                      child: const Text(
-                        'Update',
-                        style: TextStyle(color: Colors.white),
-                      ),
+                  ),
+                  ElevatedButton(
+                    onPressed: updateInformation,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue.withOpacity(.9),
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        deleteUser(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red.withOpacity(.9),
-                      ),
-                      child: const Text('Delete Account'),
+                    child: const Text(
+                      'Update',
+                      style: TextStyle(color: Colors.white),
                     ),
-                    // Blur effect when userDeleted is true
-                    if (userDeleted)
-                      Container(
-                        color: Colors.black.withOpacity(0.0),
-                        child: BackdropFilter(
-                          filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Successfully deleted the account',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
-                                  ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      deleteUser(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red.withOpacity(.9),
+                    ),
+                    child: const Text('Delete Account'),
+                  ),
+                  if (userDeleted)
+                    Container(
+                      color: Colors.black.withOpacity(0.0),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Successfully deleted the account',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
                                 ),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => Login(),
-                                      ),
-                                    );
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor:
-                                        const Color.fromARGB(255, 70, 233, 255),
-                                  ),
-                                  child: const Text('Back to Login'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Login(),
+                                    ),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromARGB(255, 70, 233, 255),
                                 ),
-                              ],
-                            ),
+                                child: const Text('Back to Login'),
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                  ],
-                ),
+                    ),
+                ],
               ),
             ),
           ),
